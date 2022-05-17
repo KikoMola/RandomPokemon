d1f66c3f92cef481aa8722fc8dbb0d7b394c3595
Kiko <kikoguirado@gmail.com>
2022-05-17 16:23:23 +0200

    added fetching svg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokemonService } from './services/pokemon.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RandomPokemon';
+  actualPokemon = '';
+  image = '';
+
+  constructor(private pokemonService: PokemonService) { }
+
+  ngOnInit(): void {
+    this.getRandomPokemon();
+    // console.log(this.actualPokemon);
+  }
+  
+  getRandomPokemon() {
+    this.pokemonService.getRandomPokemon(Math.floor(Math.random() * 807) + 1).subscribe(
+      (data: any) => {
+        this.actualPokemon = data.name;
+        this.image = data.sprites.other.dream_world.front_default;
+        console.log(this.actualPokemon);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
+
